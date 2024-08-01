08f6baf4267fc4eb2473ec273d0a00c500a66880
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 15:53:13 +0300

    Add project files.

---
+++ Klavyeden kenarı girilen küpün alanını hesaplama/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klavyeden_kenarı_girilen_küpün_alanını_hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Formül : 6*a*a
+
+
+            Console.WriteLine("Küpün bir kenarını giriniz");
+            int a = Int32.Parse(Console.ReadLine());
+
+            int alan = 6 * a * a;
+
+            Console.WriteLine("Küpün alanı= " + alan);
+            Console.ReadLine();
+        }
+    }
+}
